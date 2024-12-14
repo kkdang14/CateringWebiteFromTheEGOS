0d736868a84ff52cb93797400fa0037ba2609f05
Admin <kkdang2707.dev@gmail.com>
2024-12-14 19:21:11 +0700

    update connection db

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2/promise");
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const mySqlPool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: '',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
 })
 
 const checkConnection = async () => {
